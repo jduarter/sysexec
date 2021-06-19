eac17efeb4a53801bbdfd787b0d18d77b236574c
Jorge Duarte <info@malagadev.com>
2021-06-19 11:04:06 +0200

    build: improved rollup setup

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,20 @@
 import typescript from '@rollup/plugin-typescript';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+import cleanup from 'rollup-plugin-cleanup';
+import analyze from 'rollup-plugin-analyzer';
+import license from 'rollup-plugin-license';
+
+const path = require('path');
+
+const LICENSE_CFG = {
+  banner: {
+    content: {
+      file: path.join(__dirname, 'LICENSE'),
+    },
+  },
+};
 
 export default [
   {
@@ -10,11 +24,22 @@ export default [
       { file: 'build/lib.min.js', format: 'cjs', plugins: [terser()] },
       { file: 'build/lib.esm.js', format: 'esm' },
     ],
-    plugins: [typescript()],
+    plugins: [
+      typescript(),
+      cleanup({ comments: 'none' }),
+      nodeResolve(),
+      license(LICENSE_CFG),
+      analyze(),
+    ],
   },
   {
     input: 'src/index.ts',
     output: [{ file: 'build/lib.d.ts', format: 'es' }],
-    plugins: [dts()],
+    plugins: [
+      dts(),
+      cleanup({ comments: 'none' }),
+      license(LICENSE_CFG),
+      analyze(),
+    ],
   },
 ];
